a64beee9551323bc70d4c45a7ba105a1d23744fb
Gabrieeeel123 <104031898+Gabrieeeel123@users.noreply.github.com>
2022-12-08 21:56:09 -0500

    Add files via upload

---
+++ Reto1/Salud/Ejecucion.java
@@ -0,0 +1,11 @@
+package Salud;
+
+public class Ejecucion {
+    public static void main(String[] args) {
+    persona calcular = new persona();
+    calcular.pedirdatos();
+    calcular.mostrarpersona();
+    calcular.calcularmc();
+    calcular.mayoredad();
+    }
+}
